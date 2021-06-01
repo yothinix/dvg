1a05a3f83e2dfff08e9132c7931c7aab0af260fb
Yothin Muangsommuk <m@yothinix.com>
2021-06-01 20:58:51 +0700

    Add Post admin

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -11,3 +11,47 @@ class ProfileAdmin(admin.ModelAdmin):
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     model = Tag
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+    list_display = (
+        'id',
+        'title',
+        'subtitle',
+        'slug',
+        'publish_date',
+        'published',
+    )
+
+    list_filter = (
+        'published',
+        'publish_date',
+    )
+
+    list_editable = (
+        'title',
+        'subtitle',
+        'slug',
+        'publish_date',
+        'published',
+    )
+
+    search_fields = (
+        'title',
+        'subtitle',
+        'slug',
+        'body',
+    )
+
+    prepopulated_fields = {
+        'slug': (
+            'title',
+            'subtitle',
+        )
+    }
+
+    date_hierarchy = 'publish_date'
+    save_on_top = True
