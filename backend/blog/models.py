2838ded80465afa8e87b6f58214c8376cbe56c6e
Yothin Muangsommuk <m@yothinix.com>
2021-06-01 20:48:25 +0700

    Add tag model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -13,3 +13,9 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.get_username()
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
