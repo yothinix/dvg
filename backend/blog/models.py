643f8f86d718d03cfca1d3f290cbd49484623575
Yothin Muangsommuk <m@yothinix.com>
2021-06-01 20:52:27 +0700

    Add Post model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from django.conf import settings
 from django.db import models
 
@@ -19,3 +20,21 @@ class Tag(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Post(models.Model):
+    class Meta:
+        ordering = ['-publish_date']
+
+    title = models.CharField(max_length=255, unique=True)
+    subtitle = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    meta_description = models.CharField(max_length=150, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+
+    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
