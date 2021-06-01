294fbe77ae2c9bc5bfb018e9a11e44a2bcd0ac34
Yothin Muangsommuk <m@yothinix.com>
2021-06-01 20:47:17 +0700

    Add Profile model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
+    website = models.URLField(blank=True)
+    bio = models.CharField(max_length=240, blank=True)
+
+    def __str__(self):
+        return self.user.get_username()
+
