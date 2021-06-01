627809fe6c0880af0ed3298cdb3088d53257fdfc
Yothin Muangsommuk <m@yothinix.com>
2021-06-01 20:55:56 +0700

    Add admin for Profile, Tag

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Profile, Post, Tag
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
