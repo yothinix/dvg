e6d012fd0b48ff435ff9e524d0060e71b08b1517
Yothin Muangsommuk <m@yothinix.com>
2021-06-04 22:55:36 +0700

    Adding graphql endpoint

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,9 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  apolloProvider: createProvider(),
+  apolloProvider: createProvider({
+    httpEndpoint: 'http://localhost:8000/graphql',
+    wsEndpoint: null,
+  }),
   render: h => h(App)
 }).$mount('#app')
