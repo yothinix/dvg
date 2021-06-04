aeea7ad88864cfa2ac221c33861815b451708d32
Yothin Muangsommuk <m@yothinix.com>
2021-06-04 22:00:16 +0700

    Add routing for each components

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Post from '@/components/Post'
+import Author from '@/components/Author'
+import PostsByTag from '@/components/PostsByTag'
+import AllPosts from '@/components/AllPosts'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { paht: '/author/:username', component: Author },
+  { paht: '/post/:slug', component: Post },
+  { paht: '/tag/:slug', component: PostsByTag },
+  { path: '/', component: AllPosts },
+  { path: '/about', name: 'About', component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')}
 ]
 
 const router = new VueRouter({
